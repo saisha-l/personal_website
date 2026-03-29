c8add9cc1a2dfe44f15418ce5a9c420e71ec2f1b
Saisha <87923617+saisha-l@users.noreply.github.com>
2026-03-28 18:05:59 -0700

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
   },
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "/",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : "",
+  },
 };
 
 export default nextConfig;
